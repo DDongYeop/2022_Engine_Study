bbbf557e401e1f3566337ed54f231d1d8fd68aff
Dongyeop <kitty-777@naver.com>
2022-05-03 21:53:44 +0900

    20220503
    It's a simple shooting game made for practice.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    MeshRenderer bgMatRenderer;
+    public float scrollSpeed = 0.2f;
+    void Start()
+    {
+        bgMatRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Vector2.up;
+        bgMatRenderer.material.mainTextureOffset += direction * scrollSpeed * Time.deltaTime;
+    }
+}
