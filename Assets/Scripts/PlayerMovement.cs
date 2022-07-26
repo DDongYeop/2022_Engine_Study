d72b94d82d89b81ea7451cc96e79e2cbf4c0daa9
Dongyeop <kitty-777@naver.com>
2022-07-26 15:19:25 +0900

    InputSystem

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 _moveInput;
+
+
+    private void OnMove()
+    {
+        
+    }
+
+    private void OnFire()
+    {
+
+    }
+
+    private void OnJump()
+    {
+
+    }
+}
