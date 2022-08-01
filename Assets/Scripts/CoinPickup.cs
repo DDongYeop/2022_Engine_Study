2903411fc969770009e7164074edc5c0e9f050ae
Dongyeop <kitty-777@naver.com>
2022-08-01 16:08:01 +0900

    Coin Aidio

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coins : MonoBehaviour
+public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip _audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(_audioClip, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
