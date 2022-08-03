a25e695e8004c6307d2c8b658d022b96b27c4ee3
Dongyeop <kitty-777@naver.com>
2022-08-03 14:53:26 +0900

    답에 따른 반응

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header ("Question")]
     [SerializeField] TextMeshProUGUI questionTxt;
-    [SerializeField] GameObject[] answerButton;
     [SerializeField] QuestionSO question;
+
+    [Header ("ANswer")]
+    [SerializeField] GameObject[] answerButton;
+
+    [Header("Button Image")]
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     private void Start()
     {
@@ -19,5 +27,19 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI button = answerButton[i].GetComponentInChildren<TextMeshProUGUI>();
             button.text = question.GetAnswer(i);
         }
+    }
+
+    public void OnAnsweredSelected(int index)
+    {
+        Image thisImage = answerButton[question.CorrectAnswerIndex].GetComponent<Image>();
+        
+        if (index == question.CorrectAnswerIndex)
+            questionTxt.text = "맞습니다 ! ";
+        else
+        {
+            string correctAnswer = question.GetAnswer(question.CorrectAnswerIndex);
+            questionTxt.text = "틀렸습니다 ! \n 답은 " + correctAnswer + " 입니다!";
+        }
+        thisImage.sprite = correctAnswerSprite;
     }
 }
