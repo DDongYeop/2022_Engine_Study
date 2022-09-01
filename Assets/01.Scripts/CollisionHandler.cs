718add6bc14a2a60d04b0310223294cd6a76971f
Dongyeop <kitty-777@naver.com>
2022-09-01 18:06:49 +0900

    Map, dead
    【한글자막】 C#과 Unity로 3D 게임 개발하기 - 43 ~ 45

---
+++ Assets/01.Scripts/CollisionHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                break;
+
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if ( nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
