7467ec2c2bfa910ec7acef9ccd70641813b92e49
Dongyeop <kitty-777@naver.com>
2022-09-06 20:20:27 +0900

    Animation, Pooling

---
+++ Assets/01.Scripts/Spawner/Spawner.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnModes
+{
+    Fixed,
+    Random
+}
+
+public class Spawner : MonoBehaviour
+{
+    [Header ("SpawnMode")]
+    [SerializeField] private SpawnModes _spawnMode = SpawnModes.Fixed;
+
+    [Header ("Test")]
+    [SerializeField] private int _enemyCnt = 10;
+    [SerializeField] private GameObject _test;
+
+    [Header ("SpawnDelay")]
+    [SerializeField] private float _delayBtwSpawn;
+    [SerializeField] private float _minRandomDelay;
+    [SerializeField] private float _maxRandomDelay;
+    [SerializeField] private float _spawnTimeInt = 2;
+
+    private ObjectPooler _pooler;
+
+    private float _spawnTime;
+    private float _enemiesSpawned;
+
+    private void Awake()
+    {
+        _pooler = GetComponent<ObjectPooler>();
+    }
+
+    private void Update()
+    {
+        _spawnTime += Time.deltaTime;
+
+        if (_spawnTime > GetSpawnDelay() && _enemiesSpawned < _enemyCnt)
+        {
+            _spawnTime = 0;
+            SpawnEnemy();
+            _enemiesSpawned++;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject newInstance = _pooler.GetInstanceFromPool();
+        newInstance.SetActive(true);
+        newInstance.transform.position = transform.position;
+    }
+
+    private float GetSpawnDelay()
+    {
+        if (_spawnMode == SpawnModes.Random)
+        {
+            float randomTimer = Random.Range(_minRandomDelay, _maxRandomDelay);
+            return randomTimer;
+        }
+        
+        else if (_spawnMode == SpawnModes.Fixed)
+            return _spawnTimeInt;
+
+        else
+        {
+            Debug.LogError("Spawner의 SpawnMode이 설정 되지 않은 값입니다");
+            return 999;
+        }
+    }
+}
