ac499853aaf7361a046201ee333db4da73d0673f
Dongyeop <kitty-777@naver.com>
2022-09-11 18:43:46 +0900

    Resource
    Learn to make an Awesome Builder-Defender game in Unity! - [11 ~ 12]

--- Assets/01.Scripts/ResourceManager.cs
+++ Assets/01.Scripts/ResourceManager.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
+    public static ResourceManager Instance { get; private set; }
+
+    public event EventHandler OnResourceAmountChanged;
+
     private Dictionary<ResourceTypeSO, int> _resourceAmountDictionary;
 
     private void Awake()
     {
+        Instance = this;
+
         _resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
 
         ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
@@ -41,5 +48,14 @@ public class ResourceManager : MonoBehaviour
     public void AddResource(ResourceTypeSO resourceType, int amount)
     {
         _resourceAmountDictionary[resourceType] += amount;
+
+        OnResourceAmountChanged(this, EventArgs.Empty);
+
+        TestLogResourceAmountDictionary();
+    }
+
+    public int GetResourceAmount(ResourceTypeSO resourceType)
+    {
+        return _resourceAmountDictionary[resourceType];
     }
 }
