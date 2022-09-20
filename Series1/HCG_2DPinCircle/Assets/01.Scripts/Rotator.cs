8c1b39308ae110c09a2bb74fcf70131005f358e7
Dongyeop <kitty-777@naver.com>
2022-09-20 19:30:59 +0900

    Target
    세션0_과녁 오브젝트

---
+++ Series1/HCG_2DPinCircle/Assets/01.Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 50f;
+    [SerializeField] private Vector3 _rotateAngle = Vector3.forward;
+
+    private void Update()
+    {
+        transform.Rotate(_rotateAngle * _rotateSpeed * Time.deltaTime);
+    }
+}
