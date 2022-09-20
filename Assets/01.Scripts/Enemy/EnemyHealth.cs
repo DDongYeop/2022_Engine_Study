e7a18ea3207206bf3a4ebd759eaec0d053a9a796
Dongyeop <kitty-777@naver.com>
2022-09-20 19:20:34 +0900

    Enemy die, healthBar

---
+++ Assets/01.Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private GameObject _healthBarPrefab;
+    [SerializeField] private Transform _barPosition;
+
+    [SerializeField] private float _initiaHealth = 10f;
+    [SerializeField] private float _maxHealth = 10f;
+
+    public float currentHealth { get; set; }
+
+    private Image _healthBar;
+
+    private void Start()
+    {
+        CreateHealthBar();
+        currentHealth = _initiaHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            DealDamage(5);
+
+        _healthBar.fillAmount = Mathf.Lerp(_healthBar.fillAmount, currentHealth / _maxHealth, Time.deltaTime * 10f);
+    }
+
+    private void CreateHealthBar()
+    {
+        GameObject newBar = Instantiate(_healthBarPrefab, _barPosition.position, Quaternion.identity);
+        newBar.transform.SetParent(transform);
+
+        EnemyHealthContainer container = newBar.GetComponent<EnemyHealthContainer>();
+        _healthBar = container.fillAmountImage;
+    }
+
+    public void DealDamage(float damageReceived)
+    {
+        currentHealth -= damageReceived;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        currentHealth = _initiaHealth;
+        _healthBar.fillAmount = 1f;
+        ObjectPooler.ReturnToPool(gameObject);
+    }
+}
