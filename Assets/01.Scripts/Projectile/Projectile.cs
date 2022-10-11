ebe65fb8006e1ff30ab5fbc6daf9c8a333bfb659
DDongYeop <kitty-777@naver.com>
2022-10-11 19:30:30 +0900

    Damage text, canvas

--- Assets/01.Scripts/Projectile/Projectile.cs
+++ Assets/01.Scripts/Projectile/Projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
-    private Enemy _emenyTarget;
+    private Enemy _enenyTarget;
 
     [SerializeField] private float damage = 2f;
     [SerializeField] private float minDisToDealDamage = 0.1f;
@@ -14,7 +14,7 @@ public class Projectile : MonoBehaviour
 
     private void Update()
     {
-        if(_emenyTarget != null)
+        if(_enenyTarget != null)
         {
             MoveProjectile();
             RotateProjectile();
@@ -23,13 +23,12 @@ public class Projectile : MonoBehaviour
 
     private void MoveProjectile()
     {
-        transform.position = Vector2.MoveTowards(transform.position,
-            _emenyTarget.transform.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, _enenyTarget.transform.position, moveSpeed * Time.deltaTime);
 
-        float disToTarget = (_emenyTarget.transform.position - transform.position).magnitude;
+        float disToTarget = (_enenyTarget.transform.position - transform.position).magnitude;
         if(disToTarget < minDisToDealDamage) //적과 총알 거리 충분히 가까움. 총알 맞음
         {
-            _emenyTarget.EnemyHealth.DealDamage(damage);
+            _enenyTarget.EnemyHealth.DealDamage(damage);
 
             TurretOwner.ResetTurretProjectile();
             objectPooler.ReturnToPool(gameObject); //총알 맞으면 오브젝트 풀러로 되돌림
@@ -38,13 +37,19 @@ public class Projectile : MonoBehaviour
 
     public void SetEnemy(Enemy enemy)
     {
-        _emenyTarget = enemy;
+        _enenyTarget = enemy;
     }
 
     private void RotateProjectile()
     {
-        Vector3 enemyPos = _emenyTarget.transform.position - transform.position;
+        Vector3 enemyPos = _enenyTarget.transform.position - transform.position;
         float angle = Vector3.SignedAngle(transform.up, enemyPos, transform.forward);
         transform.Rotate(0f, 0f, angle);
+    }
+
+    public void ResetProjectile()
+    {
+        _enenyTarget = null;
+        transform.rotation = Quaternion.identity;
     }
 }
