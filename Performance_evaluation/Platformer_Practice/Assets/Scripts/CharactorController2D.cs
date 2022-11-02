1f8b3abcf19efdab2559938af6d1f8779a8fca6e
DDongYeop <kitty-777@naver.com>
2022-11-02 23:53:00 +0900

    수행평가 준비완료

--- Performance_evaluation/Platformer_Practice/Assets/Scripts/CharactorController2D.cs
+++ Performance_evaluation/Platformer_Practice/Assets/Scripts/CharactorController2D.cs
@@ -1,18 +1,18 @@
 using GlobalType;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharactorController2D : MonoBehaviour
 {
+    public float raycastDistance = 0.2f;
+    public LayerMask layerMask;
+    public float slopAngleLimit = 45f;
 
-    //flags
     public bool below;
     public bool above;
     public bool right;
     public bool left;
-     
+
     private Vector2 _moveAmount;
     private Vector2 _currentPosition;
     private Vector2 _lastPosition;
@@ -20,7 +20,9 @@ public class CharactorController2D : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private CapsuleCollider2D _capsuleCollider;
 
-    void Awake()
+    private bool _dissbleGroundCheck;
+
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _capsuleCollider = GetComponent<CapsuleCollider2D>();
@@ -28,7 +30,60 @@ public class CharactorController2D : MonoBehaviour
 
     private void Update()
     {
-    
+        _lastPosition = _rigidbody.position;
+        _currentPosition = _lastPosition + _moveAmount;
+        _rigidbody.MovePosition(_currentPosition);
+        _moveAmount = Vector2.zero;
 
-    }   
+        CheckOtherCollision();
+    }
+
+    public void Move(Vector2 movement)
+    {
+        _moveAmount += movement;
+    }
+
+    private void CheckOtherCollision()
+    {
+        RaycastHit2D leftHit = Physics2D.BoxCast(_capsuleCollider.bounds.center, _capsuleCollider.size * 0.7f, 0f, Vector2.left, raycastDistance, layerMask);
+
+        if (leftHit.collider)
+            left = true;
+        else
+            left = false;
+
+        RaycastHit2D rightHit = Physics2D.BoxCast(_capsuleCollider.bounds.center, _capsuleCollider.size * 0.7f, 0f, Vector2.right, raycastDistance, layerMask);
+        if (rightHit.collider)
+            right = true;
+        else
+            right = false;
+
+        RaycastHit2D aboveHit = Physics2D.CapsuleCast(_capsuleCollider.bounds.center, _capsuleCollider.size, CapsuleDirection2D.Vertical, 0f, Vector2.up, raycastDistance, layerMask);
+        if (aboveHit.collider)
+            above = true;
+        else
+            above = false;
+
+        if (!_dissbleGroundCheck)
+        {
+            RaycastHit2D hit = Physics2D.CapsuleCast(_capsuleCollider.bounds.center, _capsuleCollider.size, CapsuleDirection2D.Vertical, 0f, Vector2.down, raycastDistance, layerMask);
+            if (hit.collider)
+                below = true;
+            else
+                below = false;
+        }
+    }
+
+    public void DisableGroundCheck(float delayTime)
+    {
+        below = false;
+        _dissbleGroundCheck = true;
+        StartCoroutine(EnableGroundCheck(delayTime));
+    }
+
+    private IEnumerator EnableGroundCheck(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        _dissbleGroundCheck = false;
+    }
 }
--- Performance_evaluation/Platformer_Practice/Assets/Scripts/PlayerController.cs
+++ Performance_evaluation/Platformer_Practice/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //player state
+    [Header("Player Properties")]
+    public float walkSpeed = 10;
+    public float creepSpeed = 5f;
+    public float gravity = 20f;
+    public float jumpSpeed = 15f;
+    public float wallSlideAmount = 0.1f;
+    public float dashSpeed = 40f;
+    public float dashTime = .2f;
+    public float dashCooldownTime = 1f;
+
+    [Header("Player Abilities")]
+    public bool canWallSlide;
+    public bool canAirDash;
+    public bool canGroundDash;
+
     [Header("Player States")]
     public bool isJumping;
-    public bool isDashing; 
+    public bool isWallSliding;
+    public bool isDashing;
+
+    private bool _startJump;
+    private bool _realeaseJump;
 
     private Vector2 _input;
-
+    private Vector2 _moveDirection;
     private CharactorController2D _charactorController;
-
 
     private void Awake()
     {
@@ -22,11 +38,150 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        PlayerMove();
+        PlayerDash();
+        SpriteFlip();
+        PlayerJump();
 
+        dashCooldownTime -= Time.deltaTime;
     }
 
     public void OnMovement(InputAction.CallbackContext context)
     {
-        
+        _input = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            _startJump = true;
+            _realeaseJump = false;
+        }
+        else if (context.canceled)
+        {
+            _startJump = false;
+            _realeaseJump = true;
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (dashCooldownTime >= 0)
+            return;
+
+        dashCooldownTime = 1f;
+        if (context.started)
+        {
+            if ((canAirDash && !_charactorController) || (canAirDash && _charactorController))
+                StartCoroutine(Dash());
+        }
+    }
+
+    private void SpriteFlip()
+    {
+        if (_input.x > 0)
+            transform.localScale = Vector3.one;
+        else if (_input.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void PlayerJump()
+    {
+        if (_charactorController.below)
+            OnGround();
+        else 
+            InAir();
+
+
+        _charactorController.Move(_moveDirection * Time.deltaTime);
+    }
+
+    private void OnGround()
+    {
+        _moveDirection.y = 0;
+        ClearAirAbillityFlags();
+
+        Jump();
+    }
+
+    private void ClearAirAbillityFlags()
+    {
+        isJumping = false;
+        isWallSliding = false;
+    }
+
+
+    private void Jump()
+    {
+        if (_startJump)
+        {
+            _startJump = false;
+            _moveDirection.y = jumpSpeed;
+            isJumping = true;
+            _charactorController.DisableGroundCheck(0.1f);
+
+        }
+    }
+
+    private void InAir()
+    {
+        if (_realeaseJump)
+        {
+            _realeaseJump = false;
+            if (_moveDirection.y > 0)
+            {
+                _moveDirection.y *= 0.5f;
+            }
+        }
+
+        AirJump();
+        GravityCalculation();
+    }
+
+    private void AirJump()
+    {
+        if (_startJump)
+        {
+            _startJump = false;
+        }
+    }
+
+    private void GravityCalculation()
+    {
+        if (isDashing)
+            return;
+
+        if (canWallSlide && (_charactorController.left || _charactorController.right))
+        {
+            if (_moveDirection.y <= 0)
+                _moveDirection.y -= gravity * wallSlideAmount * Time.deltaTime;
+            else
+                _moveDirection.y -= gravity * Time.deltaTime;
+
+            isWallSliding = true;
+        }
+        else
+            _moveDirection.y -= gravity * Time.deltaTime;
+    }
+
+    private void PlayerMove()
+    {
+        _moveDirection.x = _input.x * walkSpeed;
+    }
+
+    private void PlayerDash()
+    {
+        if (!isDashing)
+            return;
+
+        _moveDirection.x = transform.localScale.x * dashSpeed;
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
     }
 }
