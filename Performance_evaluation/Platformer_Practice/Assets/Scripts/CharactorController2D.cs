25351223544aa8abbea1bb66fa964bef6b64fd2c
Dongyeop <kitty-777@naver.com>
2022-11-03 11:59:17 +0900

    Update CharactorController2D.cs

--- Performance_evaluation/Platformer_Practice/Assets/Scripts/CharactorController2D.cs
+++ Performance_evaluation/Platformer_Practice/Assets/Scripts/CharactorController2D.cs
@@ -16,14 +16,12 @@ public class CharactorController2D : MonoBehaviour
     private Vector2 _lastPosition;
 
     private Rigidbody2D _rigidbody;
-    private CapsuleCollider2D _capsuleCollider;
 
     private bool _dissbleGroundCheck;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
