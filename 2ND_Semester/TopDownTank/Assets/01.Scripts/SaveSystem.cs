df571ff82fc83d2e7f0e49c1e390b0fe8e6e5b82
Dongyeop <kitty-777@naver.com>
2022-12-11 15:48:40 +0900

    TopDownTank - 저장, Level분리, Cinemachine

---
+++ 2ND_Semester/TopDownTank/Assets/01.Scripts/SaveSystem.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SaveSystem : MonoBehaviour
+{
+    public string playerHealthKey = "PlayerHealth", sceneKey = "SceneIndex", savePresentKey = "SavePresent";
+    public LoadData LoadedData { get; private set; }
+
+    public UnityEvent<bool> OnDataLoadedResult;
+
+    private void Awake() 
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start() 
+    {
+        
+    }
+
+    public void ResetData() 
+    {
+        PlayerPrefs.DeleteKey(playerHealthKey);
+        PlayerPrefs.DeleteKey(sceneKey);
+        PlayerPrefs.DeleteKey(savePresentKey);
+        LoadedData = null;
+    }
+
+    public void SaceData(int sceneIndex, int playerHealth)
+    {
+        if (LoadedData == null)
+            LoadedData = new LoadData();
+
+        LoadedData.sceneIndex = sceneIndex;
+        LoadedData.playerHealth = playerHealth;
+        
+        PlayerPrefs.SetInt(playerHealthKey, playerHealth);
+        PlayerPrefs.SetInt(sceneKey, sceneIndex);
+        PlayerPrefs.SetInt(savePresentKey, 1);
+    }
+
+    public bool LoadData()
+    {
+        if (PlayerPrefs.GetInt(savePresentKey) == 1)
+        {
+            LoadedData = new LoadData();
+            LoadedData.playerHealth = PlayerPrefs.GetInt(playerHealthKey);
+            LoadedData.sceneIndex = PlayerPrefs.GetInt(sceneKey);
+            return true;
+        }
+        return false;
+    }
+}
+
+public class LoadData
+{
+    public int playerHealth = -1;
+    public int sceneIndex = -1;
+}
