ebc6a8b8024c5b180f4b9a3a3d3b4578d8320474
Dongyeop <kitty-777@naver.com>
2023-02-04 23:23:22 +0900

    UIToolkitLecture - Flex Study (여러개 나오게)
    2023.01.30 (02:17:30)

--- Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/FlexDocument/FlexStudy.cs
+++ Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/FlexDocument/FlexStudy.cs
@@ -15,8 +15,22 @@ public class FlexStudy : MonoBehaviour
         
         toggleBtn.RegisterCallback<ClickEvent>(evt =>
         {
-            VisualElement card = root.Q<VisualElement>(className: "card"); //클래스 이름이 card인 걸 가져와 1개만
-            card.AddToClassList("on");
+            // VisualElement card = root.Q<VisualElement>(className: "card"); //클래스 이름이 card인 걸 가져와 1개만
+            List<VisualElement> cardList = root.Query<VisualElement>(className: "card").ToList(); //클래스 이름이 card인 걸 가져와 여러개를 
+            // 한개만 가져올거면 Q를 쓰고 여러개 가져올일이 있다면 Quary을 쓴다. 
+
+            cardList.ForEach(card =>
+            {
+                if (card != null && card.ClassListContains("on"))
+                {
+                    card?.RemoveFromClassList("on");
+                    // card.AddToClassList("off");
+                }
+                else 
+                    card?.AddToClassList("on");
+
+            });
+
             // 덮어쓸떄 만약에 인라인으로 스타일이 지정되어있다면 절대 못 덮어쓴다. (변경해돈게 우선적) 
         });
     }
