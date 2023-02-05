1586620f68fdff50007d31526b0daf0df7268d8b
Dongyeop <kitty-777@naver.com>
2023-02-05 15:24:08 +0900

    UIToolkitLecture - ChatDialog (채팅 생성 되게)
    2023.01.31 (00:58:18)

--- Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/ChatDialog/ChatDialog.cs
+++ Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/ChatDialog/ChatDialog.cs
@@ -6,10 +6,14 @@ using UnityEngine.UIElements;
 
 public class ChatDialog : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset _chatTemplate;
+    [SerializeField] private Sprite _profileSprite;
+    
     private VisualElement _root;
     private UIDocument _document;
     private int _showIndex;
     private List<VisualElement> _chatList;
+    private ScrollView _scrollView;
 
     private void Awake()
     {
@@ -21,6 +25,7 @@ public class ChatDialog : MonoBehaviour
         _root = _document.rootVisualElement;
         _showIndex = 0;
         _chatList = _root.Query<VisualElement>(className: "chat").ToList();
+        _scrollView = _root.Q<ScrollView>("ScrollViewChat"); // 스크롤 뷰를 가져오고 
     }
 
     private void Update()
@@ -28,17 +33,36 @@ public class ChatDialog : MonoBehaviour
         // 여기서 스페이스 누를 때마다 List에 showIndex번쨰 녀석에게 on을 붙여주고 //리스트에 더이상 아이템이 없다면 
         // 모든 녀석에게서 on을 때라 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     if (_showIndex >= _chatList.Count)
+        //         CLearListOn();
+        //     else
+        //     {
+        //         _chatList[_showIndex].AddToClassList("on");
+        //         _showIndex++;
+        //         
+        //     }
+        // }
+
+        if (Input.GetButtonDown("Jump"))
         {
-            if (_showIndex >= _chatList.Count)
-                CLearListOn();
-            else
+            VisualElement chat = _chatTemplate.Instantiate();
+            chat = chat.Q("ChatMsg");
+            StartCoroutine(DelayCorutine(0.1f, () =>
             {
-                _chatList[_showIndex].AddToClassList("on");
-                _showIndex++;
-                
-            }
+                chat.AddToClassList("on");
+                _scrollView.verticalScroller.ScrollPageDown(1);
+                // _scrollView.verticalScroller.value = _scrollView.verticalScroller.highValue;
+            }));
+            _scrollView.contentContainer.Add(chat);
         }
+    }
+
+    private IEnumerator DelayCorutine(float time, Action Callback)
+    {
+        yield return new WaitForSeconds(time);
+        Callback();
     }
 
     private void CLearListOn()
