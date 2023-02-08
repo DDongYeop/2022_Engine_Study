a83e4d892c9f14b49562c46971c56890abfcf9ea
Dongyeop <kitty-777@naver.com>
2023-02-08 17:41:27 +0900

    UIToolkitLecture - DragAndDeop
    2023.02.01 (02:16:00)

---
+++ Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/DragAndDrop/PlayerController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private int _maxHP;
+    private int _hp;
+
+    public UnityEvent<float> UpdateHealth;
+    
+    private void Awake()
+    {
+        _hp = _maxHP;
+    }
+
+    public void ChangeHealth(int value)
+    {
+        _hp = Mathf.Clamp(_hp + value, 0, _maxHP);
+        UpdateHealth?.Invoke(_hp / (float)_maxHP); // 이건 UI에 값을 갱신 하는거 
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(x, y) * (Time.deltaTime * _speed);
+        
+        transform.Translate(movement);
+        
+        if (Input.GetButtonDown("Jump"))
+            ChangeHealth(-10);
+    }
+}
