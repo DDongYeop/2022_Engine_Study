34795365c23d194951bb2e5c8787e9d1b24c6154
Dongyeop <kitty-777@naver.com>
2023-02-08 20:42:21 +0900

    UIToolkitLecture - DragAndDeop (드래그앤드롭으로 체력 올리기)
    2023.02.01 (03:22:23)

--- Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/DragAndDrop/DragAndDrop.cs
+++ Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/DragAndDrop/DragAndDrop.cs
@@ -22,6 +22,29 @@ public class DragAndDrop : MonoBehaviour
         _potion = root.Q<VisualElement>("Potion");
         
         //여기서 메뉴플레이어가 나옴 
+        _potion.AddManipulator(new Dragger(PotionDeop));
+    }
+
+    private void PotionDeop(Vector2 startPos, Vector2 endPos)
+    {
+        //드롭된 위치에 플레이어가 있는지 판단해서 있다면 해당 플레이어의 체력 업 
+        Vector2 endScreenPos = new Vector2(endPos.x, Screen.height - endPos.y);
         
+        Ray ray = _mainCamera.ScreenPointToRay(endScreenPos);
+        RaycastHit hit;
+        int playerLayer = LayerMask.NameToLayer("Player");
+
+        bool isRayHit = Physics.Raycast(ray, out hit, _mainCamera.farClipPlane, 1 << playerLayer);
+
+        if (isRayHit)
+        {
+            PlayerController p = hit.collider.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                _potion.parent.Remove(_potion); //VisualElement를 삭제할 때는 부모로부터 삭제하면 잘 된다. 
+                                                //실제 삭제가 아니라 노드트리에서만 뺴는거다. 메모리에서 삭제되는거 아님. _potion은 여전히 잘 가르키고 있다
+                p.ChangeHealth(20);
+            }
+        }
     }
 }
--- Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/DragAndDrop/Dragger.cs
+++ Unity/WInterVacation/UIToolkitLecture/Assets/01.Scripts/UI/DragAndDrop/Dragger.cs
@@ -1,18 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Dragger : MonoBehaviour
+public class Dragger : MouseManipulator
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _startPos; //드래그 시작 위치
+    private bool _isDragging = false;
+    private Vector2 _originalPos; //원본 위치
+
+    private Action<Vector2, Vector2> _dropCallback; //드랍되었을 때 실행할 콜백
+
+
+    public Dragger(Action<Vector2, Vector2> DropCallback)
     {
-        
+        _dropCallback = DropCallback; // 드롭 콜백 넣어주고 
+        _isDragging = false;
+        activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
+    }
+    
+    protected override void RegisterCallbacksOnTarget()
+    {
+        //타겟이 활성화되었을 때 달아줘야하는 콜백 이벤트
+        target.RegisterCallback<MouseDownEvent>(OnMouseDown);
+        target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
+        target.RegisterCallback<MouseUpEvent>(OnMouseUp);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void UnregisterCallbacksFromTarget()
     {
+        //타겟이 비활성화되었을 때 제거해야 하는 콜백이벤트들
+        target.RegisterCallback<MouseDownEvent>(OnMouseDown);
+        target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
+        target.RegisterCallback<MouseUpEvent>(OnMouseUp);
+    }
+    
+    // 이 이름은 정해진거 아님
+    protected void OnMouseDown(MouseDownEvent evt)
+    {
+        if (CanStartManipulation(evt)) //지덩된 activator의 조건등을 만족하는 검사한다. 
+        {
+            _startPos = evt.localMousePosition;
+            _originalPos = new Vector2(target.style.left.value.value, target.style.top.value.value);
+            _isDragging = true;
+            
+            //여기에 뭔가 하나 더해야하는데 일단 안 하고 
+            target.CaptureMouse(); //마우스를 잡겠다.
+            evt.StopPropagation();
+        }
+    }
+    
+    protected void OnMouseMove(MouseMoveEvent evt)
+    {
+        if (_isDragging && target.HasMouseCapture())
+        {
+            Vector2 diff = evt.localMousePosition - _startPos; //이동거리가 측정된다(이번 이벤트 프레임간)
+
+            target.style.top = new Length(target.layout.y + diff.y, LengthUnit.Pixel);
+            target.style.left = new Length(target.layout.x + diff.x, LengthUnit.Pixel);
+        }
+    }
+    
+    protected void OnMouseUp(MouseUpEvent evt)
+    {
+        if (!_isDragging || !target.HasMouseCapture())
+            return;
         
+        target.ReleaseMouse(); //릴리즈해서 풀어주고
+        _isDragging = false;
+        _dropCallback?.Invoke(_originalPos, evt.mousePosition);
     }
 }
